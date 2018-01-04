8b75ea60a74fe55dce899ce83681179f62e049d9
suhasjadhav23 <suhasjadhav23@gmail.com>
2018-01-04 19:19:25 +0530

    payment gateway added

--- sales/admin.py
+++ sales/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from sales.models import Sale
+from django.contrib import admin
 # Register your models here.
+admin.site.register(Sale)
